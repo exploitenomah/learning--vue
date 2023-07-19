efb028ef0d20ffdf87d94b586ee7abef8da5e8eb
exploitenomah <iksploiting@gmail.com>
2023-07-19 04:08:21 +0100

    refactor to allow local storage in absence of json-server

--- src/App.vue
+++ src/App.vue
@@ -13,32 +13,18 @@ export default {
     handleToggleShowForm() {
       this.showForm = !this.showForm
     },
-    handleFormChangeEvent(e) {
-      const { name, value, type, checked } = e.target
-      if (type === 'checkbox') this.formValues = { ...this.formValues, [name]: checked }
-      else this.formValues = { ...this.formValues, [name]: value }
-    },
-    resetFormValues() {
-      this.formValues = {
-        ...this.formValues, date: '',
-        reminder: false,
-        time: '',
-        description: ''
-      }
-    },
     async addTask(newTask) {
       this.tasks = [...this.tasks, await this.createTask({ ...newTask, id: Math.random() })]
-      this.resetFormValues()
     },
     async removeTask(taskId) {
       const isDeleted = await this.deleteTask(taskId)
-      if (isDeleted) this.tasks = this.tasks.filter(task => task._id === taskId)
+      if (isDeleted) this.tasks = [...this.tasks].filter(task => task.id !== taskId)
       else alert('Error deleting task')
     },
     async toggleReminder(taskId) {
       const taskToUpdate = await this.fetchTask(taskId)
-      const update = await this.updateTask({ ...taskToUpdate, reminder: !taskToUpdate.reminder })
-      if (update) this.tasks = this.tasks.map(task => task._id === update.id ? update : task)
+      const updates = await this.updateTask({ ...taskToUpdate, reminder: !taskToUpdate.reminder })
+      if (updates) this.tasks = updates
       else alert('Error updating task')
     },
     async fetchTasks() {
@@ -93,37 +79,39 @@ export default {
             'Content-type': 'application/json',
           }
         })
-        return await res.json()
+        if (res.status === 200) return await this.fetchTasks()
+        else {
+          const tasksInLsStr = localStorage.getItem('tasks')
+          const tasksInLs = JSON.parse(tasksInLsStr)
+          const modifiedTasksForLs = [...(tasksInLs || this.tasks).map(tsk => tsk.id === update.id ? update : tsk)]
+          localStorage.setItem('tasks', JSON.stringify(modifiedTasksForLs))
+          return modifiedTasksForLs
+        }
       }
       catch (err) {
-        const tasksInLsStr = localStorage.getItem('tasks')
-        const tasksInLs = JSON.parse(tasksInLsStr)
-        const modifiedTasksForLs = [...(tasksInLs || []).map(tsk => tsk.id === update.id ? update : tsk)]
-        localStorage.setItem('tasks', JSON.stringify(modifiedTasksForLs))
-        return update
+        console.error(err)
       }
     },
     async deleteTask(taskId) {
-      const res = await fetch(`/api/tasks/${taskId}`, {
-        method: 'DELETE'
-      })
-      if (res.status === 200) return res.status === 200
-      else {
-        const tasksInLsStr = localStorage.getItem('tasks')
-        const tasksInLs = JSON.parse(tasksInLsStr)
-        const modifiedTasksForLs = (tasksInLs || []).filter(tsk => tsk.id === taskId)
-        localStorage.setItem('tasks', JSON.stringify(modifiedTasksForLs))
-        return true
+      try {
+        const res = await fetch(`/api/tasks/${taskId}`, {
+          method: 'DELETE'
+        })
+        if (res.status === 200) return res.status === 200
+        else {
+          const tasksInLsStr = localStorage.getItem('tasks')
+          const tasksInLs = JSON.parse(tasksInLsStr)
+          const modifiedTasksForLs = (tasksInLs || this.tasks).filter(tsk => tsk.id !== taskId)
+          console.log(tasksInLs, modifiedTasksForLs)
+          localStorage.setItem('tasks', JSON.stringify(modifiedTasksForLs))
+          return true
+        }
+      } catch (err) {
+        return false
       }
     }
   },
   data: () => ({
-    formValues: {
-      date: '',
-      time: '',
-      reminder: false,
-      description: ''
-    },
     tasks: [],
     showForm: false
   }),
@@ -134,20 +122,16 @@ export default {
 </script>
   
 <template>
-  <Header title='Task Tracker' @toggle-show-form='handleToggleShowForm' />
-  <TaskList @toggle-reminder='toggleReminder' @delete-task='removeTask' :tasks='tasks' :show='tasks.length > 0' />
-  <div v-show="tasks.length === 0">Tasks added will be shown here</div>
-  <TaskForm :show='showForm' @change-event='handleFormChangeEvent' @add-task='addTask' />
+  <div class='h-screen overflow-hidden flex justify-center items-center'>
+    <div
+      class='h-screen relative overflow-auto w-[95vw] max-h-[90vh] max-w-[500px] mx-auto flex items-center flex-col border border/blue-600/30'>
+      <Header :showForm='showForm' title='Task Tracker' @toggle-show-form='handleToggleShowForm' />
+      <main className='w-full px-4 pb-5'>
+        <TaskForm :show='showForm' @add-task='addTask' />
+        <TaskList @toggle-reminder='toggleReminder' @delete-task='removeTask' :tasks='tasks' :show='tasks.length > 0' />
+        <div v-show="tasks.length === 0">Tasks added will be shown here</div>
+      </main>
+    </div>
+  </div>
 </template>
 
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
