286cead766623151fc3c58b52d99e8ee3df5f047
exploitenomah <iksploiting@gmail.com>
2023-07-19 04:10:34 +0100

    create styles

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -13,12 +13,15 @@ export default {
 </script>
 
 <template>
-  <ul>
-    <li v-for="task in tasks" :key="task.id">
-      <TaskItem @toggle-my-reminder='$emit("toggle-reminder", task.id)' @delete-me='$emit("delete-task", task.id)'
-        :task='task' />
-    </li>
-  </ul>
+  <div>
+    <h2 className='text-center text-2xl capitalize mt-4 mb-2'>Tasks</h2>
+    <ul className='gap-y-4 flex flex-col'>
+      <li v-for="task in tasks" :key="task.id">
+        <TaskItem @toggle-my-reminder='$emit("toggle-reminder", task.id)' @delete-me='$emit("delete-task", task.id)'
+          :task='task' />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped></style>
