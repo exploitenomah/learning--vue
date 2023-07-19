7a5bca1c0ff5a064ae12f68819a5064275a615de
exploitenomah <iksploiting@gmail.com>
2023-07-19 04:09:33 +0100

    create styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,29 +2,20 @@
 <script>
 import Button from './Button.vue'
 export default {
-  name: 'HeaderComponent', props: { title: String },
+  name: 'HeaderComponent', props: { title: String, showForm: Boolean },
   components: {
     Button
   },
-  methods: {
-
-  }
 }
 
 </script>
 
 <template>
-  <header>
-    <h1>{{ title }}</h1>
-    <Button text='Add Task' color='green' @on-click="$emit('toggle-show-form')" />
+  <header class='sticky z-10 top-0 bg-white flex w-full border-b justify-between items-center max-h-[12vh] px-4 py-5'>
+    <h1 class='text-4xl font-normal capitalize tracking-normal'>{{ title }}</h1>
+    <Button :text='showForm ? "Close" : "Add Task"' :bgColorClass='showForm ? "bg-red-500" : "bg-green-600"'
+      classes='text-white px-3 py-2 rounded-md capitalize text-sm font-semibold whitespace-nowrap  transition-all duration-[500ms] '
+      @on-click="$emit('toggle-show-form')" />
   </header>
 </template>
 
-<style scoped>
-header {
-  display: flex;
-  justify-content: space - between;
-  align-items: center;
-  margin-bottom: 20px;
-}
-</style>
